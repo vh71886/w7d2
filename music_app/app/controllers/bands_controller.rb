88fad69629434ba51556766b0c598d0da3b4c8f8
Vera Ho <vh71886@gmail.com>
2022-05-10 15:24:16 -0400

    updated bands routes and controller

---
+++ music_app/app/controllers/bands_controller.rb
@@ -0,0 +1,50 @@
+class BandsController < ApplicationController
+    def index
+        @bands = Band.all
+        render :index
+    end
+
+    def new
+        @band = Band.new
+        render :new
+    end
+
+    def create
+        @band = Band.new(band_params)
+        
+        if @band.save
+            redirect_to band_url(@band)
+        else
+            # try again
+            render :new
+        end
+    end
+
+    def edit
+        @band = Band.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @band = Band.find(params[:id])
+        if @band.update(band_params)
+            redirect_to band_url(@band)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @band = Band.find(params[:id])
+        if @band.destroy
+            redirect_to bands_url
+        else
+            redirect_to band_url(@band)
+        end
+    end
+
+    private
+    def band_params
+        params.require(:band).permit(:name)
+    end
+end
--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -1,15 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # Prefix Verb   URI Pattern                                                                              Controller#Action
-  # new_session GET    /session/new(.:format)                                                              sessions#new
-  #     session DELETE /session(.:format)                                                                  sessions#destroy
-  #             POST   /session(.:format)                                                                  sessions#create
-  #       users POST   /users(.:format)                                                                    users#create
-  #    new_user GET    /users/new(.:format)                                                                users#new
-  #        user GET    /users/:id(.:format)                                                                users#show
+  # Prefix Verb   URI Pattern                                                                            Controller#Action
+  # new_session GET    /session/new(.:format)                                                             sessions#new
+  #     session DELETE /session(.:format)                                                                 sessions#destroy
+  #             POST   /session(.:format)                                                                 sessions#create
+  #       users POST   /users(.:format)                                                                   users#create
+  #    new_user GET    /users/new(.:format)                                                               users#new
+  #        user GET    /users/:id(.:format)                                                               users#show
+  #       bands GET    /bands(.:format)                                                                   bands#index
+  #             POST   /bands(.:format)                                                                   bands#create
+  #    new_band GET    /bands/new(.:format)                                                               bands#new
+  #   edit_band GET    /bands/:id/edit(.:format)                                                          bands#edit
+  #        band GET    /bands/:id(.:format)                                                               bands#show
+  #             PATCH  /bands/:id(.:format)                                                               bands#update
+  #             PUT    /bands/:id(.:format)                                                               bands#update
+  #             DELETE /bands/:id(.:format)                                                               bands#destroy
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :bands
 
   root to: redirect('/session/new')
 end
