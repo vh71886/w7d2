56436505e0b815afbe96f1efb7a64e3483b7d4ce
Vera Ho <vh71886@gmail.com>
2022-05-10 10:08:35 -0400

    Set up user model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,2 +1,34 @@
 class User < ApplicationRecord
+    validates :email, :password_digest, :session_token, presence: true
+    validatess :email, uniqueness: true
+
+    # Create session
+    def self.generate_session_token
+
+    end
+
+    # Find user by email and password combination
+    def find_by_credentials(email, password)
+
+    end
+
+    # Set password and password_digest
+    def password=(password)
+
+    end
+
+    # Verify login password with username
+    def is_password?(password)
+
+    end
+
+    # Reset it
+    def reset_session_token!
+
+    end
+
+    # Prevent session_token = nil
+    def ensure_session_token
+
+    end
 end
