24fe9f306fa251009919c16e869b52df7ca8a3be
Vera Ho <vh71886@gmail.com>
2022-05-10 11:44:17 -0400

    fix create

--- music_app/app/controllers/users_controller.rb
+++ music_app/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
 
-        if @user   # user found
+        if @user.save
             log_in_user(@user)
             redirect_to new_user_url
         else 
