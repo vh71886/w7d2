399b442383615f5cc2726bcf57f74002b07f7bab
Vera Ho <vh71886@gmail.com>
2022-05-10 11:36:26 -0400

    add url route notes

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Prefix Verb   URI Pattern                                                                              Controller#Action
+  # new_session GET    /session/new(.:format)                                                              sessions#new
+  #     session DELETE /session(.:format)                                                                  sessions#destroy
+  #             POST   /session(.:format)                                                                  sessions#create
+  #       users POST   /users(.:format)                                                                    users#create
+  #    new_user GET    /users/new(.:format)                                                                users#new
+  #        user GET    /users/:id(.:format)                                                                users#show
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
