6779629e21535c19b4a82fa62d0e3939af1fd026
Vera Ho <vh71886@gmail.com>
2022-05-10 10:35:42 -0400

    completed auth methods

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,34 +1,50 @@
 class User < ApplicationRecord
+    attr_reader :password
+
     validates :email, :password_digest, :session_token, presence: true
-    validatess :email, uniqueness: true
+    validates :email, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    before_validation :ensure_session_token
 
     # Create session
     def self.generate_session_token
-
+        self.session_token = SecureRandom::urlsafe_base64
     end
 
     # Find user by email and password combination
-    def find_by_credentials(email, password)
+    def self.find_by_credentials(email, password)
+        @user = User.find_by(
+            params[:user][:email],
+            params[:user][:password]
+        )
 
+        if @user.nil?
+            nil
+        else
+            @user
+        end
     end
 
     # Set password and password_digest
     def password=(password)
-
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     # Verify login password with username
     def is_password?(password)
-
-    end
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end 
 
     # Reset it
     def reset_session_token!
-
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
     end
 
     # Prevent session_token = nil
     def ensure_session_token
-
+        self.session_token ||= SecureRandom::urlsafe_base64
     end
 end
