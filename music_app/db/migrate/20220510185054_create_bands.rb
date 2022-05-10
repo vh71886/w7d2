a167882bca851b5bd66f61ecb9b17fda7e4f7d3e
Vera Ho <vh71886@gmail.com>
2022-05-10 14:53:26 -0400

    completed band model and table

--- music_app/db/migrate/20220510185054_create_bands.rb
+++ music_app/db/migrate/20220510185054_create_bands.rb
@@ -1,9 +1,10 @@
 class CreateBands < ActiveRecord::Migration[5.2]
   def change
     create_table :bands do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+    add_index :bands, :name, unique: true
   end
 end
