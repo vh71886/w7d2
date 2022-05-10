21e0bdc7d098aadb5c17b88d21cd199197cc632d
Vera Ho <vh71886@gmail.com>
2022-05-10 11:44:50 -0400

    Complete create and new

--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+
+        if @user
+            log_in_user(@user)
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+
+
+    end
 end
