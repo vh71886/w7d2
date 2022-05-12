426d60b723f6efdd82836f17506be7dcea463e5a
Vera Ho <vh71886@gmail.com>
2022-05-12 09:14:38 -0400

    Add associations for band and album

--- music_app/app/models/album.rb
+++ music_app/app/models/album.rb
@@ -1,2 +1,11 @@
 class Album < ApplicationRecord
+    validates :band_id, :year, :title, :studio, presence: true
+    validates :band_id, uniqueness: true
+
+    # done
+    belongs_to :band,
+        foreign_key: :band_id,
+        class_name: :Band,
+        primary_key: :id
+
 end
--- music_app/app/models/band.rb
+++ music_app/app/models/band.rb
@@ -1,3 +1,10 @@
 class Band < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+
+    # for Album:band
+    has_many :albums,
+        foreign_key: :band_id,
+        class_name: :Album,
+        primary_key: :id
+
 end
